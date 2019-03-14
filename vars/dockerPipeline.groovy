3ddc1930c949d9eccfaad45e38d2e8042914309e
jasonluck <jluck@meetveracity.com>
2019-03-14 16:43:25 -0400

    Starting to build docker pipeline

---
+++ vars/dockerPipeline.groovy
@@ -0,0 +1,29 @@
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+        agent {
+            node {
+                label 'docker'
+            }
+        }
+        stages {
+            stage('Building Containers from Compose file') {
+                when {
+                    anyOf {
+                        fileExists 'docker-compose.yaml'
+                        fileExists 'docker-compose.yml'
+                    }
+                    
+                }
+                steps {
+                    echo "Building using compose file..."
+                }
+            }
+        }
+    }
+}
