70bdbf0414b2b71c0146762d17a2c3f2960eb6ff
jasonluck <jluck@meetveracity.com>
2019-03-19 12:57:36 -0400

    Let dockerBuild code handle default param checking

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -5,23 +5,6 @@ def call(body) {
     body.delegate = config
     body()
 
-    if (config.dockerBuilds == null) {
-        error "dockerBuilds parameter was not specified. This is required. Ex: dockerBuilds = [imageName : contextDirectory]"
-    }
-
-    if (config.dockerRegistry == null) {
-        config.dockerRegistry = env.DOCKER_REGISTRY ?: ''
-    }
-    if (config.dockerRegistryCredentialsId == null) {
-        config.dockerRegistryCredentialsId = 'docker-registry'
-    }
-    if (config.dockerHost == null) {
-        config.dockerHost = env.DOCKER_HOST
-    }
-    if (config.version == null) {
-        config.version = env.BRANCH_NAME
-    }
-
     pipeline {
         agent {
             node {
