f60af95ba4ec0608e5fde2fce442d30e9fee7fc3
jasonluck <jluck@meetveracity.com>
2019-03-20 11:20:19 -0400

    only wait 3 minutes for sonar

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -26,7 +26,7 @@ def call() {
     } else {
         this.scan()
     }
-    timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
+    timeout(time: 3, unit: 'MINTUES') { // Just in case something goes wrong, pipeline will be killed after a timeout
         def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
         if (qg.status != 'OK') {
             error "Pipeline aborted due to Sonar quality gate failure: ${qg.status}"
